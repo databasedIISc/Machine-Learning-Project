e2aacafdf284efbc5ce57d2f07eb9994adc62e9a
Ayush Raina <ayushraina@iisc.ac.in>
2023-07-16 04:25:35 +0530

    added manual encoder for now and next pages

--- app.py
+++ app.py
@@ -313,9 +313,10 @@ def miss_fill():
 #Encoding Categorical Features
 @app.route("/phase3")
 def phase3():
+    global send
     x=df.dtypes.astype(str).to_list()
     count = 0
-    idx=[]
+    idx=[] #idx of categorical features
     unique_features=[]
     for i in range(len(x)):
         if x[i]=="object":
@@ -323,12 +324,52 @@ def phase3():
             idx.append(i)
             unique_features.append(list(df[df.columns.to_list()[i]].unique()))
     if count==0:
-        return render_template("Encoding.html", message1="No Categorical Features Found",message2="Your can proceed to next step")
-
-    
+        return render_template("Encoding1.html", message1="No Categorical Features Found",message2="Your can proceed to next step")
+    feature_names=[df.columns.to_list()[i] for i in idx] #categorical feature names
+    send={} # dictionary of categorical features and their unique values
+    for i in range(len(feature_names)):
+        send[feature_names[i]]=unique_features[i]
     return render_template("Encoding.html", message1="Your dataset has "+str(count)+" categorical features",message2="Encoding them to Numeric Values here"
-                           ,x=count,feature_names=[df.columns.to_list()[i] for i in idx],unique_features=unique_features)
+                           ,send=send)
 
+@app.route("/encoding",methods=["GET","POST"])
+def encode():
+    global encodings,feature
+    encoded_values=[] #list of encoded values
+    array=[] #list of categorical features
+    feature=[] #list of categorical features
+    for features,values in send.items():
+        array.append(send[features])
+        encoded_values.append([request.form.get(f"{value}") for value in values])
+        
+    encoded_values=[[int(x) if x is not None else None for x in sub_list] for sub_list in encoded_values]
+    child_list=[] #list of index and encoded values
+    x=[sublist for sublist in encoded_values if None not in sublist]
+    child_list.append([encoded_values.index(x[0]),x[0]])
+    encodings={} #dictionary of encoded values
+    for i in range(len(child_list[0][1])):
+        encodings[array[child_list[0][0]][i]]=child_list[0][1][i]
+    for features in send.keys():
+        feature.append(features)
+    feature=feature[child_list[0][0]]
+    
+    return render_template("Encoding2.html",encodings=encodings)
+
+@app.route("/encode_it",methods=["GET","POST"])
+def encode_1():
+    
+    df[feature]=[encodings[x] for x in df[feature]]
+    return redirect(url_for("phase3"))
+
+@app.route("/phase4")
+def phase4():
+    return render_template("EDA.html")
+
+
+@app.route("/phase5")
+def phase5():
+    return render_template("ML_intro.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")
+
