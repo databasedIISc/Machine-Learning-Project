27f1b4ffb2e58b21e9bb74a120e8033b0058b348
Ayush Raina <ayushraina@iisc.ac.in>
2023-07-20 12:57:26 +0530

    fixed small mistakes

--- app.py
+++ app.py
@@ -412,6 +412,7 @@ def phase4():
 
 @app.route("/phase5")
 def phase5():
+    
     return render_template("ML_intro.html")
 
 @app.route("/show_tts")
@@ -457,21 +458,12 @@ def start_machine():
     
 @app.route("/train_reg_models", methods = ["GET","POST"])
 def train_reg_models():
-    global regression_models, linear_regressor, ridge_regressor, lasso_regressor, elastic_net_regressor
+    global regression_models, linear_regressor
     regression_models=request.form.getlist("regression_models")
     for i in regression_models:
         
         if i == "linear_reg":
             linear_regressor=linear_regression(X_train,y_train)
-            
-        if i == "ridge_reg":
-            ridge_regressor=ridge_regression(X_train,y_train)
-            
-        if i == "lasso_reg":
-            lasso_regressor=lasso_regression(X_train,y_train)
-            
-        if i == "elastic_net":
-            elastic_net_regressor=elastic_net_regression(X_train,y_train)
             
     return render_template("regression2.html")
 
@@ -490,23 +482,9 @@ def test_reg_models():
         if i == "linear_reg":
             linear_reg_pred=linear_regressor.predict(X_test)
             linear_reg_score = check_r2_score(y_test,linear_reg_pred)
-            
-        if i == "ridge_reg":
-            ridge_reg_pred=ridge_regressor.predict(X_test)
-            ridge_reg_score = check_r2_score(y_test,ridge_reg_pred)
-            
-        if i == "lasso_reg":
-            lasso_reg_pred=lasso_regressor.predict(X_test)
-            lasso_reg_score = check_r2_score(y_test,lasso_reg_pred)
-            
-        if i == "elastic_net":
-            elastic_net_pred=elastic_net_regressor.predict(X_test)
-            elastic_net_score = check_r2_score(y_test,elastic_net_pred)
-            
         
     return render_template("regression2.html",training=X_train.shape,
-                           testing=X_test.shape, accuracy_linear_reg = linear_reg_score,accuracy_ridge_reg = ridge_reg_score,
-                           accuracy_lasso_reg = lasso_reg_score, accuray_elastic_net=elastic_net_score)
+                           testing=X_test.shape, accuracy_linear_reg = linear_reg_score)
     
     
 @app.route("/test_cls_models", methods = ["GET","POST"])
