f30917372366938f4c36df921a6bf8391c79b8c7
Sirjan Hansda <sirjanhansda@iisc.ac.in>
2023-07-21 18:51:34 +0530

    Binary Logistical regression implemented

--- app.py
+++ app.py
@@ -112,6 +112,13 @@ def extra_tree_classification(X_train,y_train):
     
     return tree
     
+# Logistic Regression
+def logistic_regression(X_train,y_train):
+    
+    from sklearn.linear_model import LogisticRegression
+    log_reg=LogisticRegression(random_state=0)
+    log_reg.fit(X_train,y_train)
+    return log_reg
     
 #Home Page
 @app.route("/")
@@ -611,6 +618,7 @@ def test_decision_tree_reg():
     return jsonify({"score":score})
 
 
+
 @app.route("/train_cls_models", methods = ["GET","POST"])
 def train_cls_models():
     global classification_models
@@ -625,7 +633,25 @@ def train_cls_models():
             return render_template("models/LogisticalRegression/Logistic.html",
                                       target=target, trains=training)
         
-        
+
+@app.route("/train_logistic_regression_classifier", methods = ["GET","POST"])
+def train_logistic_regression_classifier():
+    global logistic_regression_classifier
+    logistic = request.form.get("logistic")
+    
+    if logistic == "Binary":
+        logistic_regression_classifier=logistic_regression(X_train,y_train)
+        return render_template("models/LogisticalRegression/Logistic.html",
+                                target=target, trains=training,train_status="Binary Logistic Model is trained Successfully")
+
+
+@app.route("/test_logistical_regression_classifier", methods = ["GET","POST"])
+def test_logistical_regression_classifier():
+    
+    score=check_accuracy(y_test,logistic_regression_classifier.predict(X_test))
+    score=score*100
+    return jsonify({"score":score})
+
 
 
 @app.route("/train_decision_tree_classifier", methods = ["GET","POST"])
